61b1ed490a4fdf73e194c91fceade998cf734f89
Markus Braun <markus.braun@em.ag>
2022-08-11 10:49:12 +0200

    chore(deps): updated doxygen awesome to 2.0.3
    Signed-off-by: Markus Braun <markus.braun@em.ag>

--- external/get_doxygen_awesome.sh
+++ external/get_doxygen_awesome.sh
@@ -11,5 +11,11 @@
 
 # Script to get-doxygen-awesome (full repo, not only the css file like it's stored here at present)
 echo ">>>>> cloning doxygen-awesome repo..."
-git clone https://github.com/jothepro/doxygen-awesome-css --depth=1
-rm -rf doxygen-awesome-css/.git
+VERSION=2.0.3
+curl -sSL https://github.com/jothepro/doxygen-awesome-css/archive/refs/tags/v$VERSION.tar.gz | \
+tar -xzvf - --strip-components=1 -C doxygen-awesome-css \
+doxygen-awesome-css-$VERSION/doxygen-awesome.css \
+doxygen-awesome-css-$VERSION/LICENSE
+
+#git clone https://github.com/jothepro/doxygen-awesome-css.git
+#curl -o doxygen-awesome.css https://github.com/jothepro/doxygen-awesome-css/blob/v2.0.3/doxygen-awesome.css
